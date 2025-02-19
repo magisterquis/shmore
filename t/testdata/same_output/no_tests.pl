a441ba0698ba10ffe1c3b9cd85fc22a4aa34d6bd
Stuart <jsmcmurrayjr@gmail.com>
2025-02-20 00:28:30 +0100

    Add some output, for just in case

--- t/testdata/same_output/no_tests.pl
+++ t/testdata/same_output/no_tests.pl
@@ -4,9 +4,11 @@
 # Don't actually run any tests
 # By J. Stuart McMurray
 # Created 20250218
-# Last Modified 20250218
+# Last Modified 20250220
 
 use warnings;
 use strict;
 
 use Test::More;
+
+print "Hello, World!\n";
--- t/testdata/same_output/no_tests.sh
+++ t/testdata/same_output/no_tests.sh
@@ -4,8 +4,10 @@
 # Don't actually run any tests
 # By J. Stuart McMurray
 # Created 20250218
-# Last Modified 20250218
+# Last Modified 20250220
 
 set -e
 
 . ./shmore.subr
+
+echo "Hello, World!"
