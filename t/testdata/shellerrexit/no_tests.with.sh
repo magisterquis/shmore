7e1f322c2c896bd28ff1e36958b6cbadbb633c73
Stuart <jsmcmurrayjr@gmail.com>
2025-02-20 00:31:02 +0100

    Make sure having no tests and no plan doesn't unfail a failing script

---
+++ t/testdata/shellerrexit/no_tests.with.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# no_tests.with.sh
+# No tests at all
+# By J. Stuart McMurray
+# Created 20250220
+# Last Modified 20241110
+
+set -e
+
+. ./shmore.subr
+
+r99() { return 99; }
+r99
+
+# vim: ft=sh
---
+++ t/testdata/shellerrexit/no_tests.without.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# no_tests.without.sh
+# no_tests.with.sh without shmore
+# By J. Stuart McMurray
+# Created 20250219
+# Last Modified 20241109
+
+set -e
+
+
+
+r99() { return 99; }
+r99
+
+# vim: ft=sh
